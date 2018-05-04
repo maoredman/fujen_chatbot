607248309be7b254fde69374392cb219fb4ac491
maoredman <ccm29cam@gmail.com>
2018-05-04 23:17:49 +0800

    add config.js to gitignore

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	channelSecret: '__INSERT SECRET HERE__',
-	accessToken: '__INSERT TOKEN HERE__',
+	channelSecret: '041cf5361f9dbb6772aa53210f40eadd',
+	accessToken: 'VQ4pwTIVEQH/ex8yo7affoz4Y8Oa0bch+rLdwDgT1CsrJQAJoKYcW+t9k3yMY7/USiIFvbYlZJAKTjAnK4fhJh/3sLPInbeicu4Xnz8aughw/Mm1mZVgqFkOzye4wgnN+7ckYgRsrCsAFelT9KiHZQdB04t89/1O/w1cDnyilFU=',
 };
